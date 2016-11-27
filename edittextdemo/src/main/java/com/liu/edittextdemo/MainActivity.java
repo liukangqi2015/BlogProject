d2dd48a639615880cd8781de4a3fd1fc235eb032
lkq <330620143@qq.com>
2016-11-28 00:38:57 +0800

    添加EditTextDemo

--- edittextdemo/src/main/java/com/liu/edittextdemo/MainActivity.java
+++ edittextdemo/src/main/java/com/liu/edittextdemo/MainActivity.java
@@ -1,13 +1,46 @@
 package com.liu.edittextdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button button,btn02;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setListener();
+    }
+
+    private void initView() {
+        editText= (EditText) findViewById(R.id.et);
+        button= (Button) findViewById(R.id.btn);
+        textView= (TextView) findViewById(R.id.tv);
+        btn02= (Button) findViewById(R.id.btn02);
+    }
+
+    private void setListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content=editText.getText().toString();
+                textView.setText(content);
+            }
+        });
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+            }
+        });
     }
 }
---
+++ edittextdemo/src/main/java/com/liu/edittextdemo/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.liu.edittextdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 第二个演示的Activity
+ * Created by liu on 2016/11/27 0027.
+ */
+
+public class SecondActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
